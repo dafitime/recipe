f8130965e19dc12953bed99a9c5985096fb2e95b
dafitime <djinnerman@gmail.com>
2021-06-24 14:33:23 -0400

    commit 6-24

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,19 +2,10 @@ import React from "react"
 import { Link } from "gatsby"
 import Layout from "./components/Layout"
 
-
 export default function Home() {
   return (
-    
-     <Layout>
-      <h1>Hello World!</h1>
-      <div>
-        <Link to="/about">about</Link>
-      </div>
-      <div>
-        <Link to="/company/history">History</Link>
-      </div>
-      </Layout>
-    
+    <Layout>
+      <h1>Home Page</h1>
+    </Layout>
   )
 }
